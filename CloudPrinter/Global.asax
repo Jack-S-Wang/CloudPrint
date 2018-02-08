59be6f53e1df8470110f0db62146a469021beb59
wangkehui <13738161611@163.com>
2018-02-08 14:53:32 +0800

    添加项目文件。

---
+++ CloudPrinter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudPrinter.MvcApplication" Language="C#" %>
---
+++ CloudPrinter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudPrinter.Startup))]
+namespace CloudPrinter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
